678a0436c49bcc855d05ce746e52a5d9bcb2c3ba
Shiran Meir <shiranmeir7@gmail.com>
2020-11-03 15:00:46 +0200

    Added Store component

--- src/components/Stores/Stores.jsx
+++ src/components/Stores/Stores.jsx
@@ -1,29 +1,32 @@
-import React, { useContext, useState, useEffect } from "react";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { List, Card } from "antd";
-// import { StoreContext } from "../../StoreContext";
 import "./Stores.scss";
 
 const Stores = () => {
-  //   const [itemsStore, setItemsStore] = useContext(StoreContext);
+  const dispatch = useDispatch();
+  const myList = useSelector((state) => state.items);
+  const dataList = myList.filter((item) => item.received === false);
+
+  const stores = dataList.reduce((r, a) => {
+    r[a.store] = [...(r[a.store] || []), a];
+    return r;
+  }, {});
 
   return (
-    <div className="store-list">
-      fff
-      {/* <List
-        grid={{ gutter: 16, column: 4 }}
-        dataSource={itemsStore}
-        renderItem={(item) => (
-          <List.Item>
-            <Card title={item.values.store}>
-              <div className="store-list-card">
-                <div>{item.values.name}</div>
-                <div>{item.values.price}$</div>
-              </div>
-            </Card>
-          </List.Item>
-        )}
-      /> */}
-    </div>
+    <Card className="store">
+      {Object.keys(stores).map((store, i) => (
+        <div className="store-list" key={i}>
+          <div className="store-list-title">{store}</div>
+          {stores[store].map((item) => (
+            <div className="store-list-item">
+              <div>{item.name}</div>
+              <div>{item.price}$</div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </Card>
   );
 };
 
